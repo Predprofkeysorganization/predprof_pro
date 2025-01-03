5544249dcaa5cb98a71fc59d51e144a6ffa5a995
kheledorogh <Kelehidorogh@yandex.ru>
2025-01-03 22:20:13 +0300

    add

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from flask import *
+import logging
 
 
+# logging.basicConfig(level=logging.WARNING)
 app = Flask(__name__)
 
 
 @app.route('/')
 def base_admin():
     return render_template("base.html")
+
+
+@app.route('/registration_user')
+def registration():
+    return render_template("templates_registration/registration_base.html")
+
+
+@app.route('/registration_admin')
+def registration_admin():
+    return render_template("templates_registration/registration_admin.html")
 
 
 if __name__ == '__main__':
