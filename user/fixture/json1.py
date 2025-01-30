9e814d4c0784eabea1c4c61382a7f925926a669b
kheledorogh <Kelehidorogh@yandex.ru>
2025-01-30 19:06:27 +0300

    add

--- user/fixture/json1.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('C:/Users/kheledorogh/PycharmProjects/gitproject/inventory/user/fixture/categories.json', 'rb') as f:
-    a = json.load(f)
-
-with open('C:/Users/kheledorogh/PycharmProjects/gitproject/inventory/user/fixture/categories.json', 'w',
-          encoding='utf8') as f1:
-    json.dump(a, f1, indent=5)
